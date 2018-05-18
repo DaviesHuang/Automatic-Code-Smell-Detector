7164b3d1f78d27caa15e7038f83a5a6b522ea5f0
Yuhang <yh6714@ic.ac.uk>
2018-05-18 03:01:52 +0100

    Update fixes

--- TestCode/src/ReplaceConditionalWithPolymorphismTests/example1/SpecialNumber1.java
+++
@@ -1,9 +0,0 @@
-package ReplaceConditionalWithPolymorphismTests.example1;
-
-public class SpecialNumber1 extends SpecialNumber {
-
-    @Override
-    protected int dwdwqdc() {
-        return 1;
-    }
-}
--- src/main/java/CodeInspections/ReplaceConditionalWithPolymorphism/ReplaceConditionalWithPolymorphismFix.java
+++ src/main/java/CodeInspections/ReplaceConditionalWithPolymorphism/ReplaceConditionalWithPolymorphismFix.java
@@ -1,8 +1,12 @@
 package CodeInspections.ReplaceConditionalWithPolymorphism;
 
+import DialogProviders.ReplaceConditionalWithPolymorphismDialogsProvider;
 import com.intellij.codeInspection.LocalQuickFix;
 import com.intellij.codeInspection.ProblemDescriptor;
 import com.intellij.openapi.project.Project;
+import com.intellij.psi.PsiClass;
+import com.intellij.psi.PsiSwitchStatement;
+import com.intellij.psi.util.PsiTreeUtil;
 import org.jetbrains.annotations.Nls;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,6 +21,11 @@ public class ReplaceConditionalWithPolymorphismFix implements LocalQuickFix {
 
     @Override
     public void applyFix(@NotNull Project project, @NotNull ProblemDescriptor problemDescriptor) {
-
+        PsiSwitchStatement element = (PsiSwitchStatement) problemDescriptor.getPsiElement();
+        PsiClass psiClass = PsiTreeUtil.getParentOfType(element, PsiClass.class);
+        boolean shouldRefactor = ReplaceConditionalWithPolymorphismDialogsProvider.showStartDialog(project);
+        if (shouldRefactor) {
+            ReplaceConditionalWithPolymorphismDialogsProvider.showExtractSwitchStatementDialog(psiClass, element);
+        }
     }
 }
